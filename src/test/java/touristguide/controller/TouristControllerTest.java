ed884fe71ee553a52fbe3619ce6df0f5674a18fe
maztas-tech <mata0001@stud.kea.dk>
2024-02-28 11:02:30 +0100

    Added a new tourist controller test

---
+++ src/test/java/touristguide/controller/TouristControllerTest.java
@@ -0,0 +1,33 @@
+package touristguide.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import touristguide.model.TouristAttraction;
+import touristguide.service.TouristService;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(TouristController.class)
+class TouristControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TouristService touristService;
+
+    @Test
+    void getTouristAttractions() throws Exception {
+        mockMvc.perform(get("/attractions"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("attractionList"));
+    }
+}
