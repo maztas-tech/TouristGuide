ac7ec7f53cb1cfd9f57911cb89986cb99db6630a
maztas-tech <mata0001@stud.kea.dk>
2024-03-13 09:29:10 +0100

    Created insert into tourist attraction table sql DML

--- SQL/DML/InsertIntoTouristAttractionTable.sql
+++ SQL/DML/InsertIntoTouristAttractionTable.sql
@@ -1,4 +1,7 @@
 INSERT INTO tourist_attraction (name, description, city)
 VALUES 
 ("Jellinge sten", "Vikinge historie", "Jellinge"),
-("Rundetårn", "Overblik over København", "København");
+("Rundetårn", "Overblik over København", "København"),
+("Den lille havfrue", "Demonstrations plads", "København");
+
+SELECT * FROM tourist_attraction;
